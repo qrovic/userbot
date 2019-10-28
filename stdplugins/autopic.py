6dd783c9c43abdc6a4a122016375f2975935daeb
Ravana <47528708+ravana69@users.noreply.github.com>
2019-10-28 20:49:40 +0530

    Update autopic.py

--- stdplugins/autopic.py
+++ stdplugins/autopic.py
@@ -33,7 +33,7 @@ async def autopic(event):
     photo = "photo_pfp.png"
     while not downloader.isFinished():
         place_holder = None
-    counter = -180
+    counter = -30
     while True:
         shutil.copy(downloaded_file_name, photo)
         im = Image.open(photo)
@@ -50,7 +50,7 @@ async def autopic(event):
                 file
             ))
             os.remove(photo)
-            counter -= 180
+            counter -= 30
             await asyncio.sleep(1)
         except:
             return
