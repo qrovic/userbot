e21fc496527f741d49467ba7512c1f6c11f37ca2
Ravana <47528708+ravana69@users.noreply.github.com>
2019-10-29 22:45:52 +0530

    Update chaddi.py

--- stdplugins/chaddi.py
+++ stdplugins/chaddi.py
@@ -25,7 +25,7 @@ async def _(event):
     async with borg.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=432858024))
-              await borg.forward_messages(chat, reply_message)
+              await borg.forward_messages(chat, reply_message.media)
               response = await response 
           except YouBlockedUserError: 
               await event.reply("```Please unblock @sangmatainfo_bot and try again```")
