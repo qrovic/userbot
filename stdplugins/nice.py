3cd908bcc173d30e91de04f3476cd4e7ef47572a
Ravana <47528708+ravana69@users.noreply.github.com>
2019-10-30 12:07:21 +0530

    Update nice.py

--- stdplugins/nice.py
+++ stdplugins/nice.py
@@ -25,7 +25,7 @@ async def _(event):
     async with borg.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=432858024))
-              await borg.forward_messages(chat, reply_message)
+              await borg.send_message(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
               await event.reply("```Please unblock @sangmatainfo_bot and try again```")
@@ -33,4 +33,4 @@ async def _(event):
           if response.text.startswith("Forward"):
              await event.edit("```can you kindly disable your forward privacy settings for good?```")
           else: 
-             await borg.forward_messages(event.chat_id, response.message)
+             await borg.send_message(event.chat_id, response.message)
