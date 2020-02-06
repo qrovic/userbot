137f24d72457f21623ff6c1258ed599a115cd85c
uraniumcore <59019272+uraniumcore@users.noreply.github.com>
2020-02-06 14:33:04 +0530

    2.1

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -14,7 +14,7 @@ async def _(event):
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
         s_help_string = "****:"
-    help_string = """@UniBorg ( **Custom Built By** @ElytrA7 ) \n**Verified Account**: ✅\n**Official Website**: N/A
+    help_string = """@UniBorg ( ** ReBuilt By** @ElytrA7 ) \n**Verified Account**: ✅\n**Official Website**: N/A
 Python {}
 Telethon {}
  
