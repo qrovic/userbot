ae917b697818d6a8fe4b439f6f41f156e9cea0a3
uraniumcore <manofuranium@gmail.com>
2020-03-10 11:44:07 +0530

    Update kang.py

--- stdplugins/kang.py
+++ stdplugins/kang.py
@@ -65,7 +65,7 @@ async def _(event):
             sticker.seek(0)
             uploaded_sticker = await borg.upload_file(sticker, file_name=file_ext_ns_ion)
 
-    await event.edit("ruk BC karta hu................")
+    await event.edit("Kanging................")
 
     async with borg.conversation("@Stickers") as bot_conv:
         now = datetime.datetime.now()
@@ -117,7 +117,7 @@ async def _(event):
             await silently_send_message(bot_conv, sticker_emoji)
             await silently_send_message(bot_conv, "/done")
 
-    await event.edit(f"hogya BC! idhar [Dekh](t.me/addstickers/{packshortname})")
+    await event.edit(f"kanged [Pack](t.me/addstickers/{packshortname})")
 
 
 @borg.on(admin_cmd("packinfo"))
